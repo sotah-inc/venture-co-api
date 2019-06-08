6b9e36b60c4915be1e32c463cc086ef5784f8552
Adrian Parker <ihsw.aparker@gmail.com>
2019-06-08 17:56:35 -0400

    Update venture-co-api with code to accommodate functioning in the prod environment.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,67 @@
-import * as http from "http";
+import { Firestore } from "@google-cloud/firestore";
+import { getApp, getLogger } from "@sotah-inc/api";
 import * as process from "process";
 
-import { getApp, getLogger } from "@sotah-inc/api";
+// env-var loading
+const isGceEnv = (() => {
+  const result = process.env["IS_GCE_ENV"] || "";
+  return result === "1";
+})();
 
-// app init
-const natsHost = process.env["NATS_HOST"] || "";
-const natsPort = process.env["NATS_PORT"] || "";
-const dbHost = process.env["DB_HOST"] || "";
-const dbPassword = process.env["DB_PASSWORD"] || "";
-const isGceEnv = (process.env["IS_GCE_ENV"] || "") === "1";
-const appPort = process.env["PORT"];
+// optionally loading firestore
+const firestoreDb: Firestore | null = isGceEnv ? new Firestore() : null;
+
+const getEnvVar = (envVarName: string): string => {
+  const envVar = process.env[envVarName];
+  if (typeof envVar === "undefined") {
+    return "";
+  }
+
+  return envVar;
+};
+
+const getConnectionField = async (
+  documentFieldName: string,
+  defaultValue?: string,
+): Promise<string> => {
+  if (firestoreDb === null) {
+    return defaultValue || "";
+  }
+
+  const doc = await firestoreDb
+    .collection("connection_info")
+    .doc("current")
+    .get();
+  const data = doc.data();
+  if (typeof data === "undefined") {
+    return defaultValue || "";
+  }
+  if (!(documentFieldName in data)) {
+    return defaultValue || "";
+  }
+
+  return data[documentFieldName];
+};
+
+const getConfig = async (documentFieldName: string, envVarName: string): Promise<string> => {
+  if (firestoreDb === null) {
+    return getEnvVar(envVarName);
+  }
+
+  return getConnectionField(documentFieldName, getEnvVar(envVarName));
+};
 
 // logger init
 const logger = getLogger({ level: "debug", isGceEnv });
 
 (async () => {
+  // gathering runtime configs
+  const appPort = process.env["PORT"] || 8080;
+  const natsHost: string = await getConfig("nats_host", "NATS_HOST");
+  const natsPort: string = await getConfig("nats_port", "NATS_PORT");
+  const dbHost: string = await getConfig("db_host", "DB_HOST");
+  const dbPassword: string = await getConfig("db_password", "DB_PASSWORD");
+
   const app = await getApp({ logger, natsHost, natsPort, dbHost, dbPassword, isGceEnv });
   if (app === null) {
     logger.info("Failed to initialize app");
@@ -23,13 +70,7 @@ const logger = getLogger({ level: "debug", isGceEnv });
     return;
   }
 
-  const server = http.createServer(app);
-  server.listen(appPort, () => logger.info(`Listening on ${appPort}`));
-  process.on("SIGTERM", () => {
-    logger.info("Caught SIGTERM, closing server");
-    server.close(() => {
-      logger.info("Server closed, exiting");
-      process.exit(0);
-    });
-  });
+  logger.info("Calling listen", { appPort });
+  app.listen(appPort, () => logger.info("Listening", { port: appPort }));
+  logger.info("Called listen", { appPort });
 })();
